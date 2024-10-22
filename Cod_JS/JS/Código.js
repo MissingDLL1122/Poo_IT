5808933a0a02287755973765cf9e30278dd9e1b3
MissingDLL <seba.lopes1122@gmail.com>
2024-10-21 21:46:13 -0300

    JS desde HTML conectado

---
+++ Cod_JS/JS/Código.js
@@ -0,0 +1,20 @@
+// alert('Hola Mundo desde JS');  descomentar luego 
+
+let número = -12; //Int
+let número2 = 2.6546; //Float
+let dirección = 'San Martín 1320'; //Str
+let estado = true; //Boolean
+
+
+
+// Prueba de comentario.
+// var (designar una variable)
+//let 
+
+
+
+// Pedir el nombre del usuario
+let nombre = prompt("Por favor, ingresa tu nombre:");
+
+// Mostrar mensaje de bienvenida
+alert("¡Bienvenido, " + nombre + "!");
